ea4269475ba2fbde547ad6cac4017636f4a0646f
Gnaneswara Rao Nadiminti <gnaneswar201402@gmail.com>
2021-08-15 15:40:04 +0530

    jsfiles

---
+++ jsfiles/js2.js
@@ -0,0 +1,4 @@
+function js2(){
+        document.getElementById("ab").innerHTML = "p1 content updated";
+        document.getElementById("bc").innerHTML = "p2 content updated";
+}
---
+++ jsfiles/js3.js
@@ -0,0 +1,7 @@
+function values(){
+    document.getElementById("ab").innerHTML=("x="+6);
+    document.getElementById("bc").innerHTML=("y="+7);
+    document.getElementById("cd").innerHTML=("z="+8);
+    console.log("y="+7);
+    alert("z="+8);
+}
